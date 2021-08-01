8e8a792e59370c44edb5f496fee4b1e3447ebf3d
root <907974064@qq.com>
2021-08-01 17:04:29 +0800

    GetLocalIp 不兼容了

--- funcs.go
+++ funcs.go
@@ -12,15 +12,15 @@ func GetNowTimeStr() string {
 	return time.Unix(time.Now().Unix(), 0).Format("2006-01-02 15:04:05")
 }
 
-func GetLocalIp() string {
+func GetLocalIp() (string, error) {
 
 	conn, err := net.Dial("udp", "8.8.8.8:53")
 	if err != nil {
 		log.Printf("get local addr err:%v", err)
-		return ""
+		return "", err
 	}
 	localIp := strings.Split(conn.LocalAddr().String(), ":")[0]
-	return localIp
+	return localIp, nil
 }
 
 func GetHostName() string {
